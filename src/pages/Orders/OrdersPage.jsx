42e224a5bbb06f945122874b0af48721609566d8
ghanatherohit <228R1A6780@cmrec.ac.in>
2025-03-25 02:19:36 +0530

    navigation update

--- src/pages/Orders/OrdersPage.jsx
+++ src/pages/Orders/OrdersPage.jsx
@@ -10,7 +10,7 @@ import { getImgUrl } from '../../utils/getImgUrl'
 const OrdersPage = () => {
     window.scrollTo(0, 0);
     const { currentUser } = useAuth();
-    const { data: orders = [], isLoading, isError } = useGetOrdersByUserEmailQuery(currentUser?.email);
+    const { data: orders = [], isError } = useGetOrdersByUserEmailQuery(currentUser?.email);
     const [books, setBooks] = useState([])
 
     useEffect(() => {
@@ -26,12 +26,6 @@ const OrdersPage = () => {
     }, [])
 
     console.log(books)
-
-    if (isLoading) return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-secondary"></div>
-        </div>
-    )
 
     if (isError) return (
         <div className="flex flex-col items-center justify-center h-screen text-red-600">
